e38fd5b0937325776a03630b7bd92b15cdfe7f97
Tirondzo <AlexOkonor@gmail.com>
2019-10-22 03:04:07 +0300

    Mac fixes
    Fix that makes possible to create window at all
    And fix that makes retina looks good

--- src/main.cpp
+++ src/main.cpp
@@ -363,6 +363,7 @@ int main(int argc, char** argv) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); 
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 
     window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL basic sample", nullptr, nullptr);
@@ -417,7 +418,9 @@ int main(int argc, char** argv) {
 
     ImportSceneObjectFromFile("assets/plane.xml", scene, materials);
 
-    glViewport(0, 0, WIDTH, HEIGHT);  GL_CHECK_ERRORS;
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
     glEnable(GL_DEPTH_TEST);  GL_CHECK_ERRORS;
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
